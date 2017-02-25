0e885cdf9c0678c483f26ccefbd3ea571ca143fc
aharonly <aaronsamuellei@gmail.com>
2017-02-24 20:02:32 -0500

    Add files via upload

---
+++ dataset-constr/crop.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+
+#timer
+import time
+start_time = time.time()
+
+directory = os.getcwd()
+# read a image and crop
+crop_box = (0,75,800,300) #(left, upper, right, lower)
+
+#actual code
+for filename in os.listdir(directory):
+
+#for testing
+#im = Image.open("20161101_154630.jpg")
+
+#one tab right in actual
+    if filename.endswith(".jpg") & ~(filename.endswith("_crop.jpg")):
+        im = Image.open(filename)
+        fn = filename[:-4]
+        I = im.crop(crop_box)
+        I.save('%s_crop.jpg' % fn)
+        os.remove(filename)
+
+#timer
+print("--- %s seconds ---" % (time.time() - start_time))
