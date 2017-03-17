dd2539a5ab657d8a224173d2ef9aafac80d6faf4
Allen <allendinghamilton@gmail.com>
2017-03-17 19:28:46 -0400

    removed tfidf

--- pair2vec.py
+++ pair2vec.py
@@ -3,14 +3,39 @@ import pickle
 
 import numpy as np
 from gensim.models.keyedvectors import KeyedVectors
+from nltk import TweetTokenizer
 
 from train_tfidf import train
 
 WORD2VEC_MODEL_PATH = 'GoogleNews-Vectors-negative300.bin'
 
-INPUT_FILE = "train_sample.csv"
+INPUT_FILE = "train.csv"
 VEC_DIM = 300
-OUTPUT_FILE = "embeddings.csv"
+OUTPUT_FILE = "embeddings_train.csv"
+
+TOKEN_BLACKLIST = []
+
+tknzr = TweetTokenizer(preserve_case=False, reduce_len=False, strip_handles=False) # Twitter-aware tokenizer
+
+
+# tokenize the test
+def tokenize(text):
+    tokens = tknzr.tokenize(text)
+    tokens = [token for token in tokens if not token in TOKEN_BLACKLIST] # remove blacklisted tokens
+    return tokens
+
+
+# embed a sentence
+def question2vec(question, model):
+    embedding = np.array([.0] * VEC_DIM)  # vector representation of question
+
+    tokens = tokenize(question)
+
+    for token in tokens:
+        if token in model:
+            embedding = embedding + model[token]
+
+    return embedding
 
 
 def main():
@@ -25,54 +50,29 @@ def main():
         for row in reader:
             label = int(row['is_duplicate'])
             pair = (row['question1'], row['question2'])
-
-            print(pair)
-
+            # print(pair)
             labelled_pairs.append((label, pair))
 
-
     # train tfidf mode
-    tfidf = train([question for (label, pair) in labelled_pairs for question in pair])
-    features = tfidf.get_feature_names()
-
+    #tfidf = train([question for (label, pair) in labelled_pairs for question in pair])
+    #features = tfidf.get_feature_names()
 
     # embed question pairs, last column is label
-    embeddings = []
+    embeddings = np.empty((0, VEC_DIM + 1), float)
 
     for (label, (q1, q2)) in labelled_pairs:
-        embedding1 = np.array([.0] * VEC_DIM)
-        embedding2 = np.array([.0] * VEC_DIM)
 
-        tfidf_fit1 = tfidf.transform([q1]) # fit the review using tfidf model
-        tfidf_fit2 = tfidf.transform([q2])
+        embedding1 = question2vec(q1, w2v_model)
+        embedding2 = question2vec(q2, w2v_model)
 
-        for col in tfidf_fit1.nonzero()[1]:
-            token = features[col]
-            score = tfidf_fit1[0, col]
+        diff = np.square(embedding1 - embedding2)
 
-            # extract word2vec embedding
-            if token in w2v_model:
-                embedding1 = embedding1 + score * w2v_model[token]
+        labelled = np.append(diff, [label])
 
-        for col in tfidf_fit2.nonzero()[1]:
-            token = features[col]
-            score = tfidf_fit2[0, col]
+        embeddings = np.append(embeddings, [labelled], axis=0)
 
-            # extract word2vec embedding
-            if token in w2v_model:
-                embedding2 = embedding2 + score * w2v_model[token]
+    np.savetxt(OUTPUT_FILE, embeddings, delimiter=",")
 
-        diff = embedding1 - embedding2
-
-        labelled = diff.tolist()
-        labelled.append(label)
-        embeddings.append(labelled)
-
-    # write the vector representations to a csv
-    with open(OUTPUT_FILE, 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for embedding in embeddings:
-            writer.writerow(embedding)
 
 if __name__ == "__main__":
     main()
