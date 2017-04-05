53a6ec388b02149ce18374008cd759a6bfda8f9a
Allen <allendinghamilton@gmail.com>
2017-04-04 20:01:20 -0400

    updated

--- train_tfidf_model.py
+++ train_tfidf_model.py
@@ -1,22 +1,28 @@
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from .. import quora_question_pairs_helpers as Quroa
+from quora_question_pairs_helpers import *
 
-TRAIN_FILE = 'train.csv'
-TEST_FILE = 'test.csv'
+TRAIN_FILE = 'data/train.csv'
+TEST_FILE = 'data/test.csv'
 
 MODEL_FILE = 'models/tfidf.pkl'
 
+
 def main():
     # fit tf-idf model
-    tfidf = TfidfVectorizer(lowercase=True, max_df=0.95, min_df=2, stop_words='english', use_idf=True, tokenizer=Quroa.tokenize)
+    tfidf = TfidfVectorizer(lowercase=True,
+                            max_df=0.95,
+                            min_df=2,
+                            stop_words='english',
+                            use_idf=True,
+                            tokenizer=tokenize)
 
     if TRAIN_FILE != '':
-        tfidf.fit(Quroa.QuoraQuestions.training_set(TRAIN_FILE))
+        tfidf.fit(QuoraQuestions.training_set(TRAIN_FILE))
 
     if TEST_FILE != '':
-        tfidf.fit(Quroa.QuoraQuestions.testing_set(TEST_FILE))
+        tfidf.fit(QuoraQuestions.testing_set(TEST_FILE))
 
     joblib.dump(tfidf, MODEL_FILE)
 
