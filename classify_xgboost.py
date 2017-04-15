660e8711da1f0f7c1813a961a0e1288320e14b72
Allen <allendinghamilton@gmail.com>
2017-04-15 16:53:32 -0400

    bugs fixed; added ploting

--- classify_xgboost.py
+++ classify_xgboost.py
@@ -1,9 +1,13 @@
+import datetime
+import imp
+import importlib
+
 import pandas as pd
 import xgboost as xgb
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 
-TRAIN_DATA = 'data/train_cleaned.csv'
+TRAIN_DATA = 'input/train.csv'
 TRAIN_FEATURES = "features/train.csv"
 TEST_FEATURES = "features/test.csv"
 
@@ -35,11 +39,37 @@ def rebalance2(labelled_features):
     return balanced
 
 
+def train_test_split_rebalance(features):
+    train_features, valid_features = train_test_split(features, test_size=0.1, random_state=4242)
+
+    # rebalance validation set
+    pos_samples = valid_features[valid_features['is_duplicate'] == 1]
+    neg_samples = valid_features[valid_features['is_duplicate'] == 0]
+
+    pos_count = int(len(neg_samples) / (1 - POS_PROP) - len(neg_samples))
+    valid_features = pd.concat([neg_samples, pos_samples[:pos_count]], ignore_index=True)
+
+    # rebalance training set
+    pos_samples = pd.concat([train_features[train_features['is_duplicate'] == 1], pos_samples[pos_count:]], ignore_index=True)
+    neg_samples = train_features[train_features['is_duplicate'] == 0]
+
+    needed = int(len(pos_samples) / POS_PROP - len(pos_samples))
+    multiplier = needed // len(neg_samples)
+    remainder = needed % len(neg_samples)
+
+    train_features = pd.concat([neg_samples] * multiplier + [neg_samples[:remainder], pos_samples], ignore_index=True)
+
+    return [train_features, valid_features]
+
+
 def main():
 
     print('loading training set...')
     train_data = pd.read_csv(TRAIN_DATA)
     train_features = pd.read_csv(TRAIN_FEATURES)
+
+    print('features: {0}'.format(list(train_features)))
+
     train_features = train_features.fillna(.0)
     train_features['is_duplicate'] = train_data.is_duplicate
     #train_features['weight'] = train_data.is_duplicate.map(lambda d: POS_PROP if d else ((1 - POS_PROP) / POS_PROP))
@@ -47,14 +77,19 @@ def main():
     #print('check')
     #print('word_match accuracy:', roc_auc_score(train_features['is_duplicate'], train_features['word_match']))
 
-    print('creating cross-validation set...')
-    train_features,valid_features = train_test_split(train_features, test_size=0.2, random_state=4242)
+    print('rebalancing and creating cross-validation set...')
+    # train_features,valid_features = train_test_split(train_features, test_size=0.2, random_state=4242)
+    train_features, valid_features = train_test_split_rebalance(train_features)
 
+    '''
     print('rebalance data...')
     train_features = rebalance(train_features)
     valid_features = rebalance2(valid_features)
+    '''
+
     print('label mean in training set is {0}'.format(train_features.is_duplicate.mean()))
     print('label mean in cross-validation set is {0}'.format(valid_features.is_duplicate.mean()))
+
 
     d_train = xgb.DMatrix(train_features.drop(['is_duplicate'], axis=1),
                           label=train_features.is_duplicate)
@@ -73,6 +108,22 @@ def main():
     params["seed"] = 1632
 
     bst = xgb.train(params, d_train, 500, [(d_train, 'train'), (d_valid, 'cross-validation')], early_stopping_rounds=50, verbose_eval=10)
+
+    # saving model
+    print('saving bst model...')
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
+    bst.save_model('bst-{0}.model'.format(timestamp))
+
+    # plots
+    try:
+        imp.find_module('matplotlib')
+        imp.find_module('graphviz')
+        print('drawing plots...')
+        xgb.plot_tree(bst, num_trees=1)
+        xgb.plot_importance(bst)
+
+    except ImportError:
+        print('cannot plot, matplotlib or graphviz is not installed.')
 
     # making predictions
     print('loading testing data...')
