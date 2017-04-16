19065d37f729f8c6c8d1ee649ac7dfde4c983a7f
Allen <allendinghamilton@gmail.com>
2017-04-16 18:04:12 -0400

    updated

--- classify_xgboost.py
+++ classify_xgboost.py
@@ -7,7 +7,8 @@ TRAIN_DATA = 'input/train.csv'
 TRAIN_FEATURES = "features/train.csv"
 TEST_FEATURES = "features/test.csv"
 
-SUBMISSION_FILE = 'data/submission.csv'
+TRAIN_PREDICTION = 'data/train_pred_bst.csv'
+SUBMISSION_FILE = 'data/test_pred.csv'
 
 POS_PROP = 0.1742452565
 
@@ -35,6 +36,17 @@ def train_test_split_rebalance(features):
     return [train_features, valid_features]
 
 
+def predict(features, bst, filename):
+    p_test = bst.predict(features)
+
+    print('writing predictions...')
+    predictions = pd.DataFrame()
+    predictions['test_id'] = range(0, p_test.shape[0])
+    predictions['is_duplicate'] = p_test
+    predictions = predictions.fillna(POS_PROP)
+    predictions.to_csv(filename, index=False)
+
+
 def main():
 
     print('loading training set...')
@@ -45,35 +57,31 @@ def main():
 
     train_features = train_features.fillna(.0)
     train_features['is_duplicate'] = train_data.is_duplicate
-    #train_features['weight'] = train_data.is_duplicate.map(lambda d: POS_PROP if d else ((1 - POS_PROP) / POS_PROP))
-
-    #print('check')
-    #print('word_match accuracy:', roc_auc_score(train_features['is_duplicate'], train_features['word_match']))
 
     print('rebalancing and creating cross-validation set...')
-    train_features, valid_features = train_test_split_rebalance(train_features)
+    train_t_features, train_cv_features = train_test_split_rebalance(train_features)
 
-    print('label mean in training set is {0}'.format(train_features.is_duplicate.mean()))
-    print('label mean in cross-validation set is {0}'.format(valid_features.is_duplicate.mean()))
+    print('label mean in training set is {0}'.format(train_t_features.is_duplicate.mean()))
+    print('label mean in cross-validation set is {0}'.format(train_cv_features.is_duplicate.mean()))
 
+    d_train = xgb.DMatrix(train_t_features.drop(['is_duplicate'], axis=1),
+                          label=train_t_features.is_duplicate)
+    d_valid = xgb.DMatrix(train_cv_features.drop(['is_duplicate'], axis=1),
+                          label=train_cv_features.is_duplicate)
 
-    d_train = xgb.DMatrix(train_features.drop(['is_duplicate'], axis=1),
-                          label=train_features.is_duplicate)
-    d_valid = xgb.DMatrix(valid_features.drop(['is_duplicate'], axis=1),
-                          label=valid_features.is_duplicate)
+    params = {'objective': 'binary:logistic',
+              'eval_metric': 'logloss',
+              'eta': 0.02,
+              'max_depth': 15,
+              "subsample": 0.7,
+              "min_child_weight": 2,
+              "colsample_bytree": 0.7,
+              "silent": 1,
+              "seed": 1632
+              }
 
-    params = {}
-    params['objective'] = 'binary:logistic'
-    params['eval_metric'] = 'logloss'
-    params['eta'] = 0.02
-    params['max_depth'] = 10
-    params["subsample"] = 0.7
-    params["min_child_weight"] = 2
-    params["colsample_bytree"] = 0.7
-    params["silent"] = 1
-    params["seed"] = 1632
-
-    bst = xgb.train(params, d_train, 500, [(d_train, 'train'), (d_valid, 'cross-validation')], early_stopping_rounds=50, verbose_eval=10)
+    bst = xgb.train(params, d_train, 500, [(d_train, 'train'), (d_valid, 'cross-validation')],
+                    early_stopping_rounds=50, verbose_eval=10)
 
     # saving model
     print('saving bst model...')
@@ -94,20 +102,17 @@ def main():
     '''
 
     # making predictions
+    print('predicting training data...')
+    d_train = xgb.DMatrix(train_features.drop(['is_duplicate'], axis=1))
+    predict(d_train, bst, TRAIN_PREDICTION)
+
     print('loading testing data...')
     test_features = pd.read_csv(TEST_FEATURES)
     test_features = test_features.fillna(.0)
     d_test = xgb.DMatrix(test_features)
 
-    print('making predictions...')
-    p_test = bst.predict(d_test)
-
-    print('writing predictions...')
-    predictions = pd.DataFrame()
-    predictions['test_id'] = range(0, p_test.shape[0])
-    predictions['is_duplicate'] = p_test
-    predictions = predictions.fillna(POS_PROP)
-    predictions.to_csv(SUBMISSION_FILE, index=False)
+    print('predicting testing data...')
+    predict(d_test, bst, SUBMISSION_FILE)
 
 if __name__ == '__main__':
     main()
